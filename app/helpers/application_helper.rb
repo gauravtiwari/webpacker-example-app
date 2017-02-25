c64eda5375532acd1003047587e789747de4f676
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-02-25 18:36:51 +0000

    Add a sepatrate helper for managing stylesheets

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,29 @@
 module ApplicationHelper
+  def stylesheet_pack_tag(name, **options)
+    stylesheet_link_tag(path(name), **options)
+  end
+
+  private
+
+  def path(name)
+    if config[:dev_server_host].present?
+      "#{config[:dev_server_host]}/#{name}.css"
+    elsif config[:digesting]
+      File.join(dist_path, digested_filename)
+    else
+      File.join(dist_path, "#{name}.css")
+    end
+  end
+
+  def config
+    Rails.configuration.x.webpacker
+  end
+
+  def digested_filename
+    Webpacker::Digests.lookup(name)
+  end
+
+  def dist_path
+    config[:packs_dist_path]
+  end
 end
