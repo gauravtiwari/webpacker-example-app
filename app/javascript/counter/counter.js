2b8af5063fb8ba6c62ca3f28392bcc8c35a966e4
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-02-25 17:45:01 +0000

    Add an image to counter module

--- app/javascript/counter/counter.js
+++ app/javascript/counter/counter.js
@@ -1,12 +1,14 @@
 // A simple counter example
 // The setup will be more complicated in modern apps built using React
 
+import clockIcon from './clock.png';
 const incrementNode = document.getElementById('increment');
 const decrementNode = document.getElementById('decrement');
-const inputNode = document.getElementById('counter');
+const iconNode = document.getElementById('icon');
 
 const counter = {
   initialize() {
+    iconNode.innerHTML = `<img src="${clockIcon}" alt="counter icon" />`
     incrementNode.addEventListener('click', (event) => {
       event.preventDefault();
       const currentValue = inputNode.value;
