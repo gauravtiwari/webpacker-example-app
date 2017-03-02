96f5c29cd705682f2f33cc177b22be25cbcb4659
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-03-02 14:06:05 +0000

    Update components styles

--- app/javascript/counter/index.js
+++ app/javascript/counter/index.js
@@ -1,5 +1,6 @@
 // Initialize the counter code when DOM is ready
 import './styles/style.sass';
+import './styles/container.css';
 import counter from './components/counter';
 
 document.addEventListener('DOMContentLoaded', () => {
