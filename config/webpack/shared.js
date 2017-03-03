f65728915af269995c94a113806a9840f82b5d4a
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-03-03 08:52:33 +0000

    Add js extension as well

--- config/webpack/shared.js
+++ config/webpack/shared.js
@@ -28,7 +28,7 @@ const config = {
     rules: [
       { test: /\.coffee(\.erb)?$/, loader: 'coffee-loader' },
       {
-        test: /\.jsx(\.erb)?$/,
+        test: /\.(js|jsx)(\.erb)?$/,
         exclude: /node_modules/,
         loader: 'babel-loader',
         options: {
