081a8e2fcd7f0bbea31551573b81596eee234e62
Gaurav Tiwari <gaurav@gauravtiwari.co.uk>
2017-04-17 17:26:44 +0100

    Add custom config

---
+++ app/assets/javascript/hello_example.jsx
@@ -0,0 +1,27 @@
+// Run this example by adding <%= javascript_pack_tag 'hello_react' %> to the head of your layout file,
+// like app/views/layouts/application.html.erb. All it does is render <div>Hello React</div> at the bottom
+// of the page.
+
+import React from 'react'
+import ReactDOM from 'react-dom'
+import clockIcon from './counter/images/clock.png';
+import './entries/hello-react.sass'
+
+const Hello = props => (
+  <div className='react-app-wrapper'>
+    <img src={clockIcon} alt="clock" />
+    <p className='hello-react'>
+      Good! {props.name}!
+    </p>
+  </div>
+)
+
+Hello.defaultProps = {
+  name: 'David'
+}
+
+Hello.propTypes = {
+  name: React.PropTypes.string
+}
+
+export default Hello
---
+++ config/webpack/development.js
@@ -0,0 +1,5 @@
+module.exports = {
+  entry: {
+    reactHot: 'react-hot-loader/patch'
+  }
+}
